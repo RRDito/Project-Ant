8b7c4ae41e5c039c0528711534c0ccbc1e3fcbb0
RRDito <rromanmartin@gmail.com>
2022-10-25 02:26:53 -0400

    AntMove.cs with comments

--- Assets/Scripts/AntMove.cs
+++ Assets/Scripts/AntMove.cs
@@ -20,11 +20,13 @@ public class AntMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		target = new Vector3 (transform.up.x, transform.up.y, this.transform.position.z);
-		direction = target - this.transform.position;
-		//transform.up
-		this.transform.position += (direction.normalized*0.5f*Time.deltaTime);
-		transform.Rotate (Vector3.forward * Random.Range(-15.0f, 15.0f) * 80f * Time.deltaTime);		
+		//Testing new targeted way of moving
+		//target = new Vector3 (transform.up.x, transform.up.y, this.transform.position.z);
+		//direction = target - this.transform.position;		
+		//this.transform.position += (direction.normalized*0.5f*Time.deltaTime);
 		
+		//Random facing in a 30 degree, then move forward
+		transform.Rotate (Vector3.forward * Random.Range(-15.0f, 15.0f) * 80f * Time.deltaTime);		
+		this.transform.position += (transform.up*0.5f*Time.deltaTime);
     }
 }
