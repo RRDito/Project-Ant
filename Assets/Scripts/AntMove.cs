e8d5100d82a5bfa5bcf339b3d7baa02911e0cb4e
RRDito <rromanmartin@gmail.com>
2022-10-29 02:00:07 -0400

    Movement updated

--- Assets/Scripts/AntMove.cs
+++ Assets/Scripts/AntMove.cs
@@ -14,31 +14,54 @@ public class AntMove : MonoBehaviour
     void Start()
     {   
 	    r = this.GetComponent<RectTransform>();
+		//transform.position = new Vector3(transform.position.x, transform.position.y, 10);	
         transform.Rotate (Vector3.forward * Random.Range(-180.0f, 180.0f));		
     }
 
-    public bool isNearEdge()
+    bool isNearEdge()
 	{
 		
 		if ( (r.localPosition.x > 475.0f) || (r.localPosition.x < -475.0f) ) {return true;}
 		else if ( (r.localPosition.y > 265.0f) || (r.localPosition.y < -265.0f) ) {return true;}
 		else {return false;}
 	}
+	
+		
+	void AntMovement()
+	{
+		bool MoveReady = false;
+		//Locate Food Object
+		GameObject FoodObject = GameObject.FindWithTag("Food");
+		//Calculate the current distance between the ant and the food
+		float CurrentDistance = Vector3.Distance(this.transform.position, FoodObject.transform.position);
+		
+		Debug.Log(CurrentDistance);
+		while (MoveReady == false)
+		{
+			if (isNearEdge() == true) //facing in a fixed direction if its near edge
+			{			
+				transform.Rotate (Vector3.forward * Random.Range(0.0f, 15.0f) * 80f * Time.deltaTime);
+			}		
+			else   //Random facing in a 30 degree
+			{ 
+				transform.Rotate (Vector3.forward * Random.Range(-15.0f, 15.0f) * 80f * Time.deltaTime);
+			}		
+			//move forward evaluation
+			Vector3 EvalPoint = this.transform.position + (transform.up*moveSpeed*Time.deltaTime);
+			//check if its closer
+			float NewDistance = Vector3.Distance(this.transform.position, EvalPoint);
+			
+			//Move if NewDistance is closer
+			if (NewDistance < CurrentDistance){MoveReady = true;}	
+		}
 
+		this.transform.position += (transform.up*moveSpeed*Time.deltaTime);	
+	}
+	
+	
     // Update is called once per frame
     void Update()
     {		
-		if (isNearEdge() == true)
-		{			
-			transform.Rotate (Vector3.forward * Random.Range(0.0f, 15.0f) * 80f * Time.deltaTime);
-		}		
-		else   //Random facing in a 30 degree
-		{ 
-			transform.Rotate (Vector3.forward * Random.Range(-15.0f, 15.0f) * 80f * Time.deltaTime);
-		}		
-		//move forward
-		this.transform.position += (transform.up*moveSpeed*Time.deltaTime);
-		//adjusting
-		
+		AntMovement();		
     }
 }
